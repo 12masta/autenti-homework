cc215ad843e2ef34e0d7358ebe60e576d0a1988e
marcinstanek <stanek.marcinp@gmail.com>
2021-03-13 16:33:49 +0100

    init playwright test

--- autenti-homework/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace autenti_homework
-{
-    public class Class1
-    {
-    }
-}
---
+++ autenti-homework/InitTests.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using PlaywrightSharp;
+using Xunit;
+
+namespace autenti_homework
+{
+    public class InitTests
+    {
+        [Fact]
+        public async Task TestInit()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(false);
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("https://autenti.com");
+            await page.ScreenshotAsync(path: "screenshot.png");
+        }
+    }
+}
